2d78feefc36c24be1c9a94ec33f8065f8beb5c21
twdeveloper <twdeveloper@outlook.com>
2022-05-25 22:36:48 +0800

    v1

---
+++ WebBMI/Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.FeatureManagement;
+
+namespace WebBMI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public float BmiResult = 0;
+
+        [BindProperty]
+        public int fieldHeight { get; set; }
+        [BindProperty]
+        public int fieldWeight { get; set; }
+
+        public bool isEnabled = false;
+        public readonly IConfiguration Configuration;
+        public IndexModel(IConfiguration configuration, IFeatureManager featureManager)
+        {
+            Configuration = configuration;
+
+            if (featureManager != null)
+                isEnabled = featureManager.IsEnabledAsync("toggle01").Result;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPostCalculate()
+        {
+            float height = (float)fieldHeight / 100;
+            BmiResult = fieldWeight / (height * height);
+        }
+    }
+}
---
+++ WebBMI/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+namespace WebBMI
+{
+    public class Program
+    {
+        static string AppConfigConnStr = "________AppConfigConnStr_______";
+        public static void Main(string[] args)
+        {
+            //CreateHostBuilder(args).Build().Run();
+            //config Azure App Config FeatureManagement
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((ctx, builder) =>
+                {
+                    var settings = builder.Build();
+
+                    // This section can be used to pull feature flag configuration from Azure App Configuration
+                    builder.AddAzureAppConfiguration(o =>
+                    {
+                        o.Connect(AppConfigConnStr).ConfigureRefresh(
+                            refresh =>
+                        {
+                            refresh.Register("Page:SubTitle", refreshAll: true);
+                            refresh.SetCacheExpiration(TimeSpan.FromSeconds(5));
+                        }); ;
+
+                        o.Select(KeyFilter.Any);
+
+                        o.UseFeatureFlags(FeatureFlagconfig =>
+                        {
+                            FeatureFlagconfig.CacheExpirationInterval = TimeSpan.FromSeconds(5);
+                        });
+                    });
+                })
+                .UseStartup<Startup>();
+        }
+    }
+}
